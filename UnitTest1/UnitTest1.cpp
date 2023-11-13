0a9f722fd4927aae09326f4d8376d9d075d352a2
Serhii455 <serhii.maksymiv.ri.2023@lpnu.ua>
2023-11-14 00:51:57 +0200

    my commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Pr_8.1_REC/8.1_rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+    TEST_CLASS(UnitTest1)
+    {
+    public:
+
+        TEST_METHOD(TestMethod1)
+        {
+            {
+                char str[] = "Hello !!! world!";
+                int countResult = Count(str, 1);
+
+                Assert::AreEqual(1, countResult);
+
+                char dest[151];
+                dest[0] = '\0';
+                char* destResult = Change(dest, str, dest, 0);
+
+                char expected[] = "Hello **  world!";
+
+                Assert::AreEqual(std::string(expected), std::string(destResult));
+            }
+        }
+    };
+}
